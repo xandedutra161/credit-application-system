81f99570d3b7e5b20b8491b621d0f61e56a46862
Alexandre Dutra <alexandredutra.dev@gmail.com>
2023-12-14 00:32:08 -0400

    feat: Refatoração de código no CreditResource nos codeResponses

--- src/main/kotlin/me/dio/credit/application/system/controller/CreditResource.kt
+++ src/main/kotlin/me/dio/credit/application/system/controller/CreditResource.kt
@@ -1,6 +1,7 @@
 package me.dio.credit.application.system.controller
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
@@ -26,29 +27,29 @@ class CreditResource(
 
   @Operation(summary = "Create a new Credit", method = "POST")
   @ApiResponses(value = [
-    ApiResponse(responseCode = "200", description = "Credit added successfully"),
-    ApiResponse(responseCode = "201", description = "Credit added successfully"),
-    ApiResponse(responseCode = "400", description = "Bad Request"),
-    ApiResponse(responseCode = "401", description = "Unauthorized"),
-    ApiResponse(responseCode = "403", description = "Forbidden"),
-    ApiResponse(responseCode = "404", description = "Not Found"),
-    ApiResponse(responseCode = "500", description = "Internal Server Error"),
+    ApiResponse(responseCode = "200", description = "Credit added Successfully"),
+    ApiResponse(responseCode = "201", description = "Credit added Successfully"),
+    ApiResponse(responseCode = "400", description = "Bad Request", content = [Content()]),
+    ApiResponse(responseCode = "401", description = "Unauthorized", content = [Content()]),
+    ApiResponse(responseCode = "403", description = "Forbidden", content = [Content()]),
+    ApiResponse(responseCode = "404", description = "Not Found", content = [Content()]),
+    ApiResponse(responseCode = "500", description = "Internal Server Error", content = [Content()])
   ])
   @PostMapping
-  fun saveCredit(@RequestBody @Valid creditDto: CreditDto): ResponseEntity<String> {
-      val credit: Credit = creditService.save(creditDto.toEntity())
-      val responseMessage = "Credit ${credit.creditCode} - Customer ${credit.customer?.email} saved!"
-      return ResponseEntity.status(HttpStatus.CREATED).body(responseMessage)
+  fun saveCredit(@RequestBody @Valid creditDto: CreditDto): ResponseEntity<CreditView> {
+      val savedCredit: Credit = creditService.save(creditDto.toEntity())
+      return ResponseEntity.status(HttpStatus.CREATED).body(CreditView(savedCredit))
   }
 
-  @Operation(summary = "Find all by Customer ID", method = "GET")
+  @Operation(summary = "Find all Credits by CustomerID", method = "GET")
   @ApiResponses(value = [
     ApiResponse(responseCode = "200", description = "Find all by Customer ID Successfully"),
-    ApiResponse(responseCode = "400", description = "Bad Request"),
-    ApiResponse(responseCode = "401", description = "Unauthorized"),
-    ApiResponse(responseCode = "403", description = "Forbidden"),
-    ApiResponse(responseCode = "404", description = "Not Found"),
-    ApiResponse(responseCode = "500", description = "Internal Server Error"),
+    ApiResponse(responseCode = "201", description = "Find all by Customer ID Successfully"),
+    ApiResponse(responseCode = "400", description = "Bad Request", content = [Content()]),
+    ApiResponse(responseCode = "401", description = "Unauthorized", content = [Content()]),
+    ApiResponse(responseCode = "403", description = "Forbidden", content = [Content()]),
+    ApiResponse(responseCode = "404", description = "Not Found", content = [Content()]),
+    ApiResponse(responseCode = "500", description = "Internal Server Error", content = [Content()])
   ])
   @GetMapping
   fun findAllByCustomerId(@RequestParam(value = "customerId") customerId: Long):
@@ -60,14 +61,15 @@ class CreditResource(
     return ResponseEntity.status(HttpStatus.OK).body(creditViewList)
   }
 
-  @Operation(summary = "Find by Customer ID and Credit Code", method = "GET")
+  @Operation(summary = "Find by Credits Customer ID and Credit Code", method = "GET")
   @ApiResponses(value = [
     ApiResponse(responseCode = "200", description = "Find by Customer ID and Credit Code Successfully"),
-    ApiResponse(responseCode = "400", description = "Bad Request"),
-    ApiResponse(responseCode = "401", description = "Unauthorized"),
-    ApiResponse(responseCode = "403", description = "Forbidden"),
-    ApiResponse(responseCode = "404", description = "Not Found"),
-    ApiResponse(responseCode = "500", description = "Internal Server Error"),
+    ApiResponse(responseCode = "201", description = "Find by Customer ID and Credit Code Successfully"),
+    ApiResponse(responseCode = "400", description = "Bad Request", content = [Content()]),
+    ApiResponse(responseCode = "401", description = "Unauthorized", content = [Content()]),
+    ApiResponse(responseCode = "403", description = "Forbidden", content = [Content()]),
+    ApiResponse(responseCode = "404", description = "Not Found", content = [Content()]),
+    ApiResponse(responseCode = "500", description = "Internal Server Error", content = [Content()])
   ])
   @GetMapping("/{creditCode}")
   fun findByCreditCode(
